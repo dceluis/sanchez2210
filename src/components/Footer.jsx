35805eba6c60f0806d6d2375a6b06ad371f11615
Luis Sanchez <5464881+dceluis@users.noreply.github.com>
2025-07-07 00:10:37 -0500

    Updated Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,13 @@ function Footer() {
   return (
     <footer className="bg-white ml-64">
       <br />
-      <footer className="text-center bg-gray-50 mt-20 py-16">
+      <div className="text-center bg-gray-50 mt-20 py-16">
         <div>
           <b>Luis Sanchez</b><br />
           <p className="pt-2"><i>Website and Application Developer</i></p>
         </div>
-      </footer>
-    </>
+      </div>
+    </footer>
   );
 }
 
